0df43cef0ac37a68724acc7514ab79b42b13e0c3
Akshay <Akshay@Apples-MacBook-Pro.local>
2017-10-26 21:15:02 -0400

    first commit

---
+++ src/card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './card.css';
+
+export default function Card(){
+ const text = 'Blastoise';
+ const texth2 = 'Pikachu';
+
+ return(
+  <div className='card'>
+   <h3>{texth2}</h3>
+    {text}
+  </div>
+  )
+}
